8d4ea6bf67bb372a9d23ccf3fa4b525ae5d0b9dc
scrollshadow <zaldhypramana@gmail.com>
2026-02-23 15:41:02 +0700

    first commit

---
+++ age.cpp
@@ -0,0 +1,18 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ //program utama
+    int age; //variable local
+    int age1; //variable local
+    age = 20;
+    age1 = 15;
+    
+    cout << "halo gusti" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+}
