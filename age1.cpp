367a329877dddd9fda73d78c298f26afee5d4fb1
scrollshadow <zaldhypramana@gmail.com>
2026-03-02 15:56:32 +0700

    nambah variable

---
+++ age1.cpp
@@ -0,0 +1,18 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ //program utama
+    int age; //variable local
+    int agel; //variable local
+    age = 25;
+    agel = 15;
+    
+    cout << "gusti ganteng" << endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << agel;
+}
